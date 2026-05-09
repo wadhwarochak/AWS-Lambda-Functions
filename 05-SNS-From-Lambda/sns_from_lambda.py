5033fe96c1b1f800c5b69c75b9c34fd35ab40070
wadhwarochak <rochak.wadhwa@gmail.com>
2026-05-09 06:06:29 +0530

    Adding SNS

---
+++ 05-SNS-From-Lambda/sns_from_lambda.py
@@ -0,0 +1,27 @@
+import boto3
+import pandas as pd
+
+s3_client = boto3.client('s3')
+sns_client = boto3.client('sns')
+sns_arn = 'arn:aws:sns:us-east-1:231740516500:s3-arrival-notify'
+
+def lambda_handler(event, context):
+    # TODO implement
+    print(event)
+    try:
+        bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
+        s3_file_key = event["Records"][0]["s3"]["object"]["key"]
+        print(bucket_name)
+        print(s3_file_key)
+        resp = s3_client.get_object(Bucket=bucket_name, Key=s3_file_key)
+        print(resp['Body'])
+        df_s3_data = pd.read_csv(resp['Body'], sep=",")
+        print(df_s3_data.head())
+
+
+        message = "Input S3 File {} has been processed succesfuly !!".format("s3://"+bucket_name+"/"+s3_file_key)
+        respone = sns_client.publish(Subject="SUCCESS - Daily Data Processing",TargetArn=sns_arn, Message=message, MessageStructure='text')
+    except Exception as err:
+        print(err)
+        message = "Input S3 File {} processing is Failed !!".format("s3://"+bucket_name+"/"+s3_file_key)
+        respone = sns_client.publish(Subject="FAILED - Daily Data Processing", TargetArn=sns_arn, Message=message, MessageStructure='text')
