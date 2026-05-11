22497337178f93ced82980ad95c52455af579297
wadhwarochak <rochak.wadhwa@gmail.com>
2026-05-11 06:03:21 +0530

    Adding SQS Snippets

---
+++ 06-SQS-with-Lambda/consumer_lambda_without_trigger.py
@@ -0,0 +1,20 @@
+import json
+
+def process_sales_order(sales_order):
+    print(sales_order)  # Replace with your actual processing logic.
+
+def lambda_handler(event, context):
+    # Loop through each message that triggered the lambda function
+    print("Starting SQS Batch Process")
+    print("Messages received in current batch = ",len(event['Records']))
+    print("Event Received : ", event)
+    
+    for record in event['Records']:
+        sales_order = json.loads(record['body'])
+        process_sales_order(sales_order)
+
+    print("Ending SQS Batch Process")
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Processed sales orders from SQS!')
+    }
